f719f9e8f555e1cbfa10993e9fc246fa0201b6e1
Sajjad Asadi <lvlr.xaus@gmail.com>
2018-11-28 21:10:43 +0330

    initial commit

---
+++ CircleTransition.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import { View, Animated, Dimensions } from 'react-native'
+import * as Animatable from 'react-native-animatable'
+
+const {width} = Dimensions.get('window')
+
+class CircleTransition extends Component {
+  scale = new Animated.Value(0.00001)
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      show: false,
+      showChildren: true,
+      animating: false
+    }
+
+    this.collapse = this.collapse.bind(this)
+    this.expand = this.expand.bind(this)
+    this.toggle = this.toggle.bind(this)
+  }
+
+  render () {
+    const {backgroundColor, style, revealPositionArray} = this.props
+    return (
+      this.state.show
+        ? <View
+          style={{ ...style, overflow: 'hidden' }}
+          >
+          <Animated.View style={{
+            position: 'absolute',
+            backgroundColor: backgroundColor,
+            bottom: revealPositionArray.bottom === true ? -width / 2 : undefined,
+            left: revealPositionArray.left === true ? -width / 2 : undefined ,
+            top: revealPositionArray.top === true ? -width / 2 : undefined,
+            right: revealPositionArray.right === true ? -width / 2 : undefined,
+            width: width,
+            height: width,
+            borderRadius: width / 2,
+            transform: [{
+              scale: this.scale
+            }]
+          }} />
+          {this.state.showChildren &&
+          <Animatable.View ref={(ref) => { this.childContainer = ref }} style={{opacity: 0, flex: 1}}>
+            {this.props.children}
+          </Animatable.View>
+          }
+        </View>
+        : null
+    )
+  }
+
+  expand () {
+    const {duration} = this.props
+    if (this.state.animating === false) {
+      this.setState({show: true, animating: true}, () => {
+        Animated.timing(
+          this.scale,
+          {
+            useNativeDriver: true,
+            fromValue: 0,
+            toValue: 5,
+            duration: duration
+          }
+        ).start((e) => {
+          if (e.finished === true) {
+            this.childContainer.fadeIn(200).then(() => this.setState({animating: false}))
+          }
+        })
+      })
+    }
+  }
+
+  collapse () {
+    const {duration} = this.props
+    if (this.state.animating === false) {
+      this.setState({animating: true})
+      this.childContainer.fadeOut(200)
+
+      Animated.timing(
+        this.scale,
+        {
+          useNativeDriver: true,
+          toValue: 0,
+          duration: duration
+        }
+      ).start((e) => {
+        if (e.finished === true) {
+          this.setState({show: false, animating: false})
+        }
+      })
+    }
+  }
+
+  toggle () {
+    this.state.show === false ? this.expand() : this.collapse()
+  }
+}
+
+export default CircleTransition
---
+++ README.md
@@ -0,0 +1,53 @@
+<h1 align="center">
+<br>
+ React Native Circle Reveal View
+ </br>
+ <br>
+
+</h1>
+<p align="center">
+
+ <img  src="https://raw.githubusercontent.com/lvlrSajjad/react-native-circle-reveal-view/master/circular-reveal.gif" width="30%">
+</p>
+<h2 align="center">Usage</h2>
+
+A Simple view just reveals with circular transition written in pure js
+
+installation :  
+ ```
+ npm install react-native-animatable react-native-circle-reveal-view --save
+```
+import :
+```jsx harmony
+import CircleTransition from 'react-native-circle-reveal-view'
+```
+usage :
+```jsx harmony
+        <CircleTransition
+          ref={(ref) => this.transitedView = ref}
+          backgroundColor={'white'}
+          duration={500}
+          style={{position: 'absolute', bottom: 48, right: 0, left: 0}}
+          revealPositionArray={{bottom:true,left:true}}
+        >
+          <View style={{flexDirection: 'row', width: '100%'}}>
+            <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+              <TouchableOpacity style={{
+                backgroundColor: MaterialColors.red.C500,
+                width: 48,
+                ... // A bunch of child views
+        </CircleTransition>
+        
+        // use it like
+        onPress={() => {
+            this.transitedView.toggle()
+        }}
+        onPress={() => {
+            this.transitedView.collapse()
+        }}
+        onPress={() => {
+              this.transitedView.expand()
+        }}
+```
+
+
